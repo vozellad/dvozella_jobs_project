889a0027f30472bbe8d2f4b7effdc2bcca807f2a
vozellad <vozellad@protonmail.com>
2024-01-26 11:17:28 -0500

    Started fixing formatting issues. Adding exception for api_key being in non-public file.

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+"""Contains values to not be made pubicly available on github."""
 from secrets import api_key
+
+"""..."""
+from serpapi import GoogleSearch
+
+search = GoogleSearch({
+    "q": "coffee",
+    "location": "Austin,Texas",
+    "api_key": "<your secret api key>"
+})
+result = search.get_dict()
 
 
 def main():
@@ -8,4 +19,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
