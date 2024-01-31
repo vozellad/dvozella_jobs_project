7c4c1854b2268c32cc26128f2f4de155a53bd2cb
vozellad <vozellad@protonmail.com>
2024-01-31 16:50:01 -0500

    Switched to Google Jobs for 5 pages of results

--- main.py
+++ main.py
@@ -5,23 +5,29 @@ from secrets import api_key
 from serpapi import GoogleSearch
 
 
-def get_search_results():
+def get_search_results(page_num):
+    # 10 results per page. Every 10 results is start of page.
+    page = (page_num - 1) * 10
+
     return GoogleSearch({
-        "q": "coffee",
-        "location": "Austin,Texas",
-        "api_key": api_key
+        "engine": "google_jobs",
+        "q": "software developer",
+        "location": "Boston,Massachusetts",
+        "api_key": api_key,
+        "start": page
     }).get_dict()
 
 
-def store_search_results(results):
-    with open("results.txt", "w") as file:
+def store_search_results(results, page_num):
+    with open(f"results{page_num}.txt", "w") as file:
         for key, value in results.items():
             file.write('%s: %s\n' % (key, value))
 
 
 def main():
-    results = get_search_results()
-    store_search_results(results)
+    for page_num in range(1, 6):
+        results = get_search_results(page_num)
+        store_search_results(results, page_num)
 
 
 if __name__ == "__main__":
