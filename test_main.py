5200ef00322a948080ad6309142e331bc45b79e5
vozellad <vozellad@protonmail.com>
2024-02-05 20:38:40 -0500

    Added docstrings in test_main.py

--- test_main.py
+++ test_main.py
@@ -1,8 +1,13 @@
+"""Testing functions in main.py."""
+
+
 import pytest
 import main
 
 
 def test_main():
+    """Tests if method retrieves data using Serpapi and assures it gets at least 50 job listings."""
+
     jobs_results = []
     for page in range(5):
         jobs_results += main.get_jobs_results(page)
