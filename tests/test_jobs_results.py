0a573e3685d3bf59cf151e8cbefacd44292cd0db
vozellad <vozellad@protonmail.com>
2024-02-20 11:41:57 -0500

    Update comment

--- tests/test_jobs_results.py
+++ tests/test_jobs_results.py
@@ -6,7 +6,7 @@ import jobs_results
 
 
 def test_main():
-    """Tests if method retrieves data using Serpapi and assures it gets at least 50 job listings."""
+    """Tests if method retrieves data using Serpapi and assures it gets 50 job listings."""
 
     jobs = jobs_results.get_jobs(5)
     assert len(jobs) == 50
