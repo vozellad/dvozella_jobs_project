57e6fd843c058ed4b2e077f33492686252bd2b95
vozellad <vozellad@protonmail.com>
2024-02-25 15:17:06 -0500

    Put main() job code in new function. Comment code for testing to not use tokens. Start building GUI.

--- main.py
+++ main.py
@@ -3,21 +3,32 @@
 import jobs_db
 import jobs_results
 import jobs_excel
+import sys
+from PySide6.QtWidgets import QApplication, QWidget, QPushButton
 
 
-def main():
-    # Get job data
+def get_job_data():
     jobs = jobs_results.get_jobs(5)
     jobs_results.store_jobs(jobs)
     jobs = jobs_results.prepare_jobs_for_db(jobs)
     jobs += jobs_excel.get_jobs("Sprint3Data.xlsx")
+    return jobs
+
+
+def main():
+    # uncomment code. testing without using tokens.
+
+    # jobs = get_job_data()
 
     # Store job data
     conn, cursor = jobs_db.open_db("jobs_db.sqlite")
     jobs_db.setup_db(cursor)
-    jobs_db.insert_jobs(cursor, jobs)
+    # jobs_db.insert_jobs(cursor, jobs)
 
-    jobs_db.close_db(conn)
+    # Start GUI
+    app = QApplication(sys.argv)
+    app.aboutToQuit.connect(lambda: jobs_db.close_db(conn))
+    sys.exit(app.exec_())
 
 
 if __name__ == "__main__":
