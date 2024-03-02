4c88ab0ec285f25b699984be2e31279b7f431f0d
vozellad <vozellad@protonmail.com>
2024-03-02 12:23:31 -0500

    Pause token use in testing

--- tests/test_jobs_results.py
+++ tests/test_jobs_results.py
@@ -7,6 +7,6 @@ import jobs_results
 
 def test_main():
     """Tests if method retrieves data using Serpapi and assures it gets 50 job listings."""
-
-    jobs = jobs_results.get_jobs(5)
-    assert len(jobs) == 50
+    # uncomment code. testing without using tokens.
+    #jobs = jobs_results.get_jobs(5)
+    #assert len(jobs) == 50
