008050a4b8aac81914f50d691e274bea58cc2708
vozellad <vozellad@protonmail.com>
2024-03-03 18:53:06 -0500

    Add comments

--- mapwindow.py
+++ mapwindow.py
@@ -1,3 +1,5 @@
+"""Window displaying a map used to display locations of jobs."""
+
 import io
 
 from PySide6.QtCore import Signal
@@ -34,6 +36,15 @@ class MapWindow(QWidget):
         self.layout.addWidget(self.web_view)
 
     def init_map(self):
+        """Creates empty map. Centers view on the USA. Prepares markers to be clustered.
+
+        Keyword arguments:
+        None
+
+        Returns:
+        None
+        """
+
         self.geolocator = Nominatim(user_agent="dvozella_jobs_project")
         coordinate = (39.828175, -98.5795)
         self.jobs_map = folium.Map(
@@ -44,18 +55,39 @@ class MapWindow(QWidget):
         self.map_data_markers = MarkerCluster().add_to(self.jobs_map)
 
     def refresh_map(self):
+        """Displays new markers added.
+
+        Keyword arguments:
+        None
+
+        Returns:
+        None
+        """
+
         self.in_memory_file = io.BytesIO()
         self.jobs_map.save(self.in_memory_file, close_file=False)
         self.web_view.setHtml(self.in_memory_file.getvalue().decode())
 
     def add_locations(self, jobs):
+        """Adds new markers of job locations on map.
+
+        Keyword arguments:
+        jobs -- job data (id, title, company, location)
+
+        Returns:
+        None
+        """
+
+        # Remove all markers on map (since markers can't be removed)
         self.clear()
 
         for job_id, title, company, location in jobs:
+            # Get coordinates of location
             job_loc_geocoded = self.geocode_location(location)
             if not job_loc_geocoded:
                 continue
 
+            # Add location to map
             marker = folium.Marker(
                 location=[job_loc_geocoded.latitude, job_loc_geocoded.longitude],
                 popup=f"{title}\n\n{company}"
@@ -66,10 +98,21 @@ class MapWindow(QWidget):
         self.refresh_map()
 
     def geocode_location(self, location):
+        """Gets coordinates of location.
+
+        Keyword arguments:
+        location -- Location string
+
+        Returns:
+        Location coordinates
+        """
+
+        # Don't query for coordinates if already done so before
         if location in self.geocode_cache:
             return self.geocode_cache[location]
 
         try:
+            # Get coordinates of location
             job_loc_geocoded = self.geolocator.geocode(location)
             if job_loc_geocoded:
                 self.geocode_cache[location] = job_loc_geocoded
@@ -80,9 +123,27 @@ class MapWindow(QWidget):
         return None
 
     def closeEvent(self, event):
+        """Event of window closing.
+
+        Keyword arguments:
+        event -- Window closing
+
+        Returns:
+        None
+        """
+
         self.windowClosed.emit()
         event.accept()
 
     def clear(self):
+        """Re-build map so it has no markers.
+
+        Keyword arguments:
+        None
+
+        Returns:
+        None
+        """
+
         self.init_map()
         self.refresh_map()
